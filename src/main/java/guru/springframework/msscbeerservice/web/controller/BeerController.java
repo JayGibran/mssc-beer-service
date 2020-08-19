f0b23ecaad4284025c9c7ecfe13dac621c1c8c92
Jay Gibran <jaygibran@gmail.com>
2020-08-19 18:53:07 +0100

    Adding beer entity. closes #5

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,18 +1,23 @@
 package guru.springframework.msscbeerservice.web.controller;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import guru.springframework.msscbeerservice.web.model.BeerDTO;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 
 @RestController
 @RequestMapping("/api/v1/beer")
@@ -21,12 +26,20 @@ public class BeerController {
 	
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId){
-		return new ResponseEntity<BeerDTO>(BeerDTO.builder().build(), HttpStatus.OK);
+		return new ResponseEntity<BeerDTO>(
+				BeerDTO.builder()
+				.beerName("Guiness")
+				.beerStyleEnum(BeerStyleEnum.STOUT)
+				.price(new BigDecimal(2.0))
+				.quantityOnHand(2)
+				.build(), HttpStatus.OK);
 	}
 	
 	@PostMapping
 	public ResponseEntity<String> saveNewBeer(@RequestBody BeerDTO beerDTO){
-		return new ResponseEntity<String>(HttpStatus.CREATED);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/api/v1/beer/"+ beerDTO.getId().toString());
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{beerId}")
@@ -35,5 +48,10 @@ public class BeerController {
 	}
 	
 	
+	@DeleteMapping("{beerId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+		
+	}
 
 }
