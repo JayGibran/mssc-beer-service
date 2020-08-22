82f4a0fc1b2de2219987f0cfcb22caab086294b4
Jay Gibran <jaygibran@gmail.com>
2020-08-22 15:04:57 +0100

    adding jsoncreator example. closes #14

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -3,7 +3,12 @@ package guru.springframework.msscbeerservice.web.model;
 import java.util.List;
 
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class BeerPagedList extends PageImpl<BeerDTO> {
 
@@ -11,14 +16,28 @@ public class BeerPagedList extends PageImpl<BeerDTO> {
 	 * 
 	 */
 	private static final long serialVersionUID = 2655759673295762860L;
+	
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(@JsonProperty("content") List<BeerDTO> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
 
 	public BeerPagedList(List<BeerDTO> content, Pageable pageable, long total) {
 		super(content, pageable, total);
-		// TODO Auto-generated constructor stub
 	}
 
 	public BeerPagedList(List<BeerDTO> content) {
 		super(content);
-		// TODO Auto-generated constructor stub
 	}
 }
